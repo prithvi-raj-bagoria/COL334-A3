b0b4090a71e8d790355312564eb48c0743c817b3
Prithvi Raj <prithvirajbagoria.in@gmail.com>
2025-10-10 22:02:24 +0530

    Updated p2_topo.py

--- part2/p2_topo.py
+++ part2/p2_topo.py
@@ -3,11 +3,15 @@ from mininet.net import Mininet
 from mininet.log import setLogLevel, info
 from mininet.cli import CLI
 from mininet.node import RemoteController, OVSSwitch
+# --- Step 1: Import TCLink ---
+from mininet.link import TCLink
 
 class CustomTopo(Topo):
     def build(self):
-        bw = 10 #bandwidth in Mbps
-        # Add two switches
+        # Set the link bandwidth to 10 Mbps
+        bw = 10 
+
+        # Add switches
         s1 = self.addSwitch('s1')
         s2 = self.addSwitch('s2')
         s3 = self.addSwitch('s3')
@@ -19,23 +23,32 @@ class CustomTopo(Topo):
         h1 = self.addHost('h1')
         h2 = self.addHost('h2')
         
-        # Connect hosts to switch S1
+        # Connect hosts to switches (typically with no artificial limit)
         self.addLink(h1, s1)
         self.addLink(h2, s6)
 
-        # Connect switches with each other
-        self.addLink(s1, s2, bw=bw)
-        self.addLink(s1, s3, bw=bw)
-        self.addLink(s2, s4, bw=bw)
-        self.addLink(s3, s5, bw=bw)
-        self.addLink(s4, s6, bw=bw)
-        self.addLink(s5, s6, bw=bw)
+        # Connect switches with each other using the specified bandwidth
+        # This now works because we will tell Mininet to use TCLink
+        linkopts = dict(bw=bw)
+        self.addLink(s1, s2, **linkopts)
+        self.addLink(s1, s3, **linkopts)
+        self.addLink(s2, s4, **linkopts)
+        self.addLink(s3, s5, **linkopts)
+        self.addLink(s4, s6, **linkopts)
+        self.addLink(s5, s6, **linkopts)
         
 def run():
     """Create the network, start it, and enter the CLI."""
     topo = CustomTopo()
-    net = Mininet(topo=topo, switch=OVSSwitch, build=False, controller=None,
-              autoSetMacs=True, autoStaticArp=True)
+    # --- Step 2: Tell Mininet to use TCLink for all links ---
+    net = Mininet(topo=topo, 
+                  switch=OVSSwitch, 
+                  build=False, 
+                  controller=None,
+                  link=TCLink,  # This is the crucial addition
+                  autoSetMacs=True, 
+                  autoStaticArp=True)
+                  
     net.addController('c0', controller=RemoteController, ip="127.0.0.1", protocol='tcp', port=6633)
     net.build()
     net.start()
@@ -46,9 +59,6 @@ def run():
     info('*** Stopping network\n')
     net.stop()
 
-# Example command to run: sudo python3 part1/p1_topo.py
 if __name__ == '__main__':
-    # Set log level to display Mininet output
     setLogLevel('info')
     run()
-
